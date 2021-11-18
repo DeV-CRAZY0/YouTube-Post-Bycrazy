a78903e01f0090ae4b9d528fe19a8c7278bd8908
CRAZY✔️ <94611468+DeV-CRAZY0@users.noreply.github.com>
2021-11-18 14:12:26 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    token: "",
+    channel: "",
+    messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
+    channel_id: "",
+    watchInterval: 30000
+}
